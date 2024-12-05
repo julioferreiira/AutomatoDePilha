4a0d1d72060d838f4fa7c1fa1d0726ce93b1e118
julio <julioferreiracavalcanti@gmail.com>
2024-12-05 15:40:47 -0300

    Automato de pilha

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+
+
+public class Main {
+    public static void main(String[] args) {
+        AutomatoDePilha ap = new AutomatoDePilha();
+
+        String[] testes = {"", "01", "0011", "000111", "00001111", "0001"};
+
+        for (String teste : testes) {
+            if (ap.reconhecer(teste)) {
+                System.out.println("Entrada \"" + teste + "\" foi aceita.");
+            } else {
+                System.out.println("Entrada \"" + teste + "\" foi rejeitada.");
+            }
+        }
+    }
+}
